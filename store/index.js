4242e4d68a18168feca26b8fdbdc852d0e194ba8
Chris Halley <chrishalley86@gmail.com>
2018-11-22 14:44:19 +1000

    Cleaned up Vuex store, removed Firebase references

--- store/index.js
+++ store/index.js
@@ -6,9 +6,7 @@ import utils from '../utils/utils'
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      currentUser: null,
-      dsjEvents: [],
-      editEvent: null
+      currentUser: null
     },
     mutations: {
       setCurrentUser(state, userData) {
@@ -16,12 +14,6 @@ const createStore = () => {
       },
       addEvent(state, dsjEvent) {
         state.dsjEvents.push({dsjEvent})
-      },
-      setEditEvent(state, dsjEvent) {
-        state.editEvent = {...dsjEvent}
-      },
-      clearEditEvent(state) {
-        state.editEvent = null
       }
     },
     getters: {
@@ -33,9 +25,6 @@ const createStore = () => {
       },
       getDsjEvents(state) {
         return state.dsjEvents
-      },
-      getEditEvent(state) {
-        return state.editEvent
       }
     },
     actions: {
@@ -127,11 +116,8 @@ const createStore = () => {
         })
       },
       logUserOut(vuexContext) {
-        // Clear Cookie
         Cookie.remove('dsj_access')
-        // Unset Current User 
         vuexContext.commit('setCurrentUser', null)
-        // Push to login
         this.$router.push('/login/')
       },
       addUser(vuexContext, userData) {
@@ -161,141 +147,8 @@ const createStore = () => {
           })
           .catch(e => console.log(e))
       },
-      registerUser(vuexContext, userData) {
-        // firebase.database().ref('users/').push({
-        //   firstName: userData.firstName,
-        //   lastName: userData.lastName,
-        //   email: userData.email,
-        //   userStatus: 'Pending',
-        //   applicationDate: (new Date()).toString(),
-        //   userId: ''
-        // })
-        // .then(res => {
-        //   console.log(res)
-        //   let key = res.key
-        //   firebase.database().ref('users/').child(key).update({
-        //     userId: key
-        //   })
-        // })
-        // .catch(e => {
-        //   console.log(e)
-        // })
-      },
-      approveUser(vuexContext, userData) {
-        // // Create new user in Firebase Auth Dashboard
-        // let tempUserKey = userData.userId
-        // firebase.auth().createUserWithEmailAndPassword(userData.email, 'password')
-        // .then((res) => {
-        //   //Create matching user entry in Firebase Database
-        //   firebase.database().ref('/users/' + res.user.uid).set({
-        //     firstName: userData.firstName,
-        //     lastName: userData.lastName,
-        //     email: userData.email,
-        //     userStatus: 'Approved',
-        //     applicationDate: userData.applicationDate,
-        //     approvedDate: new Date().toString(),
-        //     userId: res.user.uid
-        //   })
-        //   // Delete old entry for pending user
-        //   firebase.database().ref('/users/' + tempUserKey).remove()
-        // })
-        // .catch(e => {
-        //   console.log(e)
-        // })
-      },
-      suspendUser(vuexContext, userData) {
-        // firebase.database().ref('/users/' + userData.userId).update({
-        //   firstName: userData.firstName,
-        //   lastName: userData.lastName,
-        //   email: userData.email,
-        //   userStatus: 'Suspended',
-        //   applicationDate: userData.applicationDate,
-        //   approvedDate: userData.approvedDate,
-        //   userId: userData.userId
-        // })
-        // .then(() => {
-        //   console.log('User suspended')
-        // })
-        // .catch(e => {
-        //   console.log(e)
-        // })
-      },
-      reinstateUser(vuexContext, userData) {
-        // console.log(userData)
-        // firebase.database().ref('/users/' + userData.userId).update({
-        //   firstName: userData.firstName,
-        //   lastName: userData.lastName,
-        //   email: userData.email,
-        //   userStatus: 'Approved',
-        //   applicationDate: userData.applicationDate,
-        //   approvedDate: userData.approvedDate,
-        //   userId: userData.userId
-        // })
-        // .then(() => {
-        //   console.log('User reinstated')
-        // })
-        // .catch(e => {
-        //   console.log(e)
-        // })
-      },
-      setTokenExpire(vuexContext, duration) {
-        setTimeout(() => {
-          console.log('setTokenExpire')
-          vuexContext.commit('clearToken')
-        }, duration * 1000)
-      },
-      getUserByEmail(vuexContext, user) {
-        // return firebase.auth().fetchSignInMethodsForEmail(user.email)
-        // .then((res) => {
-        //   return res.length > 0
-        // })
-      },
-      saveEvent(vuexContext, dsjEvent) {
-        // return new Promise((resolve, reject) => {
-        //   firebase.database().ref('/events/').push({
-        //     title: dsjEvent.title,
-        //     description: dsjEvent.description,
-        //     startDateTime: dsjEvent.startDateTime,
-        //     endDateTime: dsjEvent.endDateTime
-        //   })
-        //   .then((res) => {
-        //     let key = res.key
-        //     return firebase.database().ref('/events/').child(key).update({
-        //       id: key
-        //     })
-        //   })
-        //   .then(() => {
-        //     vuexContext.commit('addEvent', dsjEvent)
-        //     resolve()
-        //   })
-        //   .catch(() => {
-        //     reject()
-        //   })
-        // })
-      },
-      checkEventAvailability(vuexContext, eventTimes) {
-        // Check if there is any event at all between the start and end dates
-
-        // If not, return a pass
-
-        // If there is, check whether the requested event clashes with the booked event 
-        // a clash is when: 
-        // (requestedEventStartTime > bookedEventStartTime && requestedEventStartTime < bookedEventEndTime) || (requestedEventEndTime > bookedEventStartTime && requestedEventEndTime < bookedEventEndTime)
-        // If there is a clash, do not take the booking and notify the user
-        // If there is not a clash, check for sufficient time before or after the requested event for a change-over of hall
-        // If there is sufficient time, take the booking
-        // If not, inform the user of the minimum time required and offer to change start or end time
-      },
-      setEditEvent(vuexContext, dsjEvent) {
-        console.log('setEditEvent() action')
-        vuexContext.commit('setEditEvent', dsjEvent)
-      },
-      clearEditEvent(vuexContext) {
-        console.log('clearEditEvent()')
-        vuexContext.commit('clearEditEvent')
-      },
       saveImageToCloudinary(vuexContext, imageUrl) {
-        console.log(crypto.SHA1)
+        // console.log(crypto.SHA1)
         // this.$axios.post('https://api.cloudinary.com/v1_1/dtn26lvux/image/upload', {
         //   file: imageUrl,
         //   api_key: '451276742949277',
@@ -332,22 +185,6 @@ const createStore = () => {
       // }
     }
   })
-}
-
-function parseCookie(context, targetCookie) {
-  let cookies, cookiesArray, cookie 
-    cookies = context.req.headers.cookie
-    cookiesArray = cookies.split(';').map((curr) => {
-      return curr.trim()
-    })
-    cookie = cookiesArray.find((curr) => {
-      return curr.startsWith(targetCookie)
-    })
-    if (cookie) {
-      return cookie.split('=')[1]
-    } else {
-      return 
-    }
 }
 
 export default createStore
