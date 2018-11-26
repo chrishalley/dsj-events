c7e81522a22ee59d463a1411eeab3054f115d823
Chris Halley <chrishalley86@gmail.com>
2018-11-26 17:43:46 +1000

    restored trailing slash to API route

--- store/index.js
+++ store/index.js
@@ -31,7 +31,7 @@ const createStore = () => {
       login(vuexContext, authData) { // Check status of existing user account before allowing login
         return new Promise((resolve, reject) => {
           console.log(process.env.baseURL);
-          this.$axios.post(`${process.env.baseURL}users/login`, authData)
+          this.$axios.post(`${process.env.baseURL}/users/login`, authData)
           .then(res => {
             vuexContext.dispatch('setCurrentUser', res.data)
             const token = res.data.tokens.find(cur => {
